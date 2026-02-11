adb77995600584be37593eb1fcfb9999d0f86707
SoulsTogetherX <xalvarez4444@gmail.com>
2026-02-10 17:21:40 -0800

    game-setup

---
+++ untitled-magical-brawler/src/other_nodes/componets/combat/hurtbox_component.gd
@@ -0,0 +1,29 @@
+class_name HurtboxComponent extends Area2D
+
+
+#region Signals
+signal on_hit(collision : HitboxComponent, invic : InvincibilityComponent)
+#endregion
+
+
+#region External Variables
+@export var health : HealthComponent
+@export var invic : InvincibilityComponent
+#endregion
+
+
+
+#region Virtual Methods
+func _init() -> void:
+	area_entered.connect(_on_area_enter)
+#endregion
+
+
+#region Signal Methods
+func _on_area_enter(collision : HitboxComponent) -> void:
+	if collision == null:
+		return
+	
+	collision.enact_collision(health, invic)
+	on_hit.emit(collision, invic)
+#endregion
