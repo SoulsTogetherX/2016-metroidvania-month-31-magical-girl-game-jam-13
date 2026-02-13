40007e2ab1568e822bdc6d33f0b441cba721db67
SoulsTogetherX <xalvarez4444@gmail.com>
2026-02-12 18:51:02 -0800

    Fixed a problem that happened due to the branch merge

--- untitled-magical-brawler/src/other_nodes/componets/other/state_manager/movement_manager/managed_task_state.gd
+++
@@ -1,8 +0,0 @@
-@abstract
-class_name ManagedTaskState extends ManagedState
-
-
-#region Public Methods (Helper)
-func get_velocity(args : Dictionary) -> VelocityComponent:
-	return args.get(TaskManager.VELOCITY_NAME)
-#endregion
