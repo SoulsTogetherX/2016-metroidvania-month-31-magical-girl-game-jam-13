aa186eee637a8246cf1d4e455f771d44ac223b80
SoulsTogetherX <xalvarez4444@gmail.com>
2026-03-01 18:16:36 -0800

    ?

--- untitled-magical-brawler/src/other_nodes/componets/other/behavior_tree/leaf/conditional/bh_cannot_move_forward.gd
+++ untitled-magical-brawler/src/other_nodes/componets/other/behavior_tree/leaf/conditional/bh_cannot_move_forward.gd
@@ -4,13 +4,21 @@ extends ConditionLeaf
 
 #region External Variables
 @export var raycast : RayCast2D
+@export var wall_check : CollisionShape2D
 #endregion
 
 
 #region Public Methods (State Change)
 func tick(_actor: Node, _blackboard: Blackboard) -> int:
-	raycast.force_raycast_update()
-	if !raycast.get_collider():
+	if wall_check && !Utilities.manual_collide_check(
+		wall_check,
+		true, false, Constants.COLLISION.GROUND
+	).is_empty():
 		return FAILURE
+	
+	if raycast:
+		raycast.force_raycast_update()
+		if !raycast.get_collider():
+			return FAILURE
 	return SUCCESS
 #endregion
